006ac9fa3def43f40c54e771be338d0ffc8c19cc
Martin Banks <anotherbanksy@gmail.com>
2018-02-10 13:02:01 +1100

    adds electron-vue boiler plate

---
+++ src/renderer/components/LandingPage.vue
@@ -0,0 +1,19 @@
+<template>
+  <h1>Hello world</h1>
+</template>
+
+<script>
+  import SystemInformation from './LandingPage/SystemInformation'
+
+  export default {
+    name: 'landing-page',
+    components: { SystemInformation },
+    methods: {
+      
+    }
+  }
+</script>
+
+<style scoped lang="sass">
+  
+</style>
