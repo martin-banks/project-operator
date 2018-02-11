0c53611917371f3335f1eebc297ae042f6cdbdf3
Martin Banks <anotherbanksy@gmail.com>
2018-02-11 22:47:55 +1100

    adds new component for warning mesaage

---
+++ src/renderer/components/Flashes/FlashWarning.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="flash flash__warning">
+    <h4><span>⚠️ Warning</span> {{ title }}</h4>
+    <p>{{ message || '--' }}</p>
+  </div>
+</template>
+
+
+<script>
+  export default {
+    name: 'flash-warning',
+    props: [
+      'title',
+      'message'
+    ],
+    data () {
+      return {}
+    }
+  }
+</script>
+
+
+<style scoped lang="sass">
+
+</style>
